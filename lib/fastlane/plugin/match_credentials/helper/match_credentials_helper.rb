929d8f1a737fae97d2677a0faa30cb68e6ff52b0
Felix Scheinost <fesc@symentis.com>
2019-03-07 17:38:56 +0100

    #noref - update to new fastlane version (2.117.1)

--- lib/fastlane/plugin/match_credentials/helper/match_credentials_helper.rb
+++ lib/fastlane/plugin/match_credentials/helper/match_credentials_helper.rb
@@ -6,22 +6,43 @@ module Fastlane
   UI = FastlaneCore::UI unless Fastlane.const_defined?("UI")
 
   module Helper
+    # Provides a way to run a block on the unencrypted repo
+    # decrypts it before and deletes it after
     class MatchCredentialsHelper
       def self.runWithRepo(params)
         return if !block_given?
 
         load_matchfile(params)
 
-        decrypted_repo = Match::GitHelper.clone(params.fetch(:git_url),
-                                                params.fetch(:shallow_clone),
-                                                branch: params.fetch(:git_branch),
-                                                clone_branch_directly: params.fetch(:clone_branch_directly))
-        Helper::CredentialsEncrypt.new.decrypt_repo(path: decrypted_repo, git_url: params[:git_url])
+        # COPIED FROM match/runner.rb
+        # Choose the right storage and encryption implementations
+        storage = Match::Storage.for_mode("git", {
+          git_url: params[:git_url],
+          shallow_clone: params[:shallow_clone],
+          #skip_docs: params[:skip_docs],
+          git_branch: params[:git_branch],
+          #git_full_name: params[:git_full_name],
+          #git_user_email: params[:git_user_email],
+          clone_branch_directly: params[:clone_branch_directly],
+          #type: params[:type].to_s,
+          #platform: params[:platform].to_s,
+          #google_cloud_bucket_name: params[:google_cloud_bucket_name].to_s,
+          #google_cloud_keys_file: params[:google_cloud_keys_file].to_s,
+          #google_cloud_project_id: params[:google_cloud_project_id].to_s
+        })
+        storage.download
 
-        return_value = yield(params, decrypted_repo)
-        FileUtils.rm_rf(decrypted_repo)
-        # Workaround, force GitHelper to clone again so that we can delete the directory
-        Match::GitHelper.class_exec { @dir = nil }
+        # Init the encryption only after the `storage.download` was called to have the right working directory
+        encryption = Helper::CredentialsEncrypt.new(
+          keychain_name: params[:git_url],
+          working_directory: storage.working_directory
+        )
+        encryption.decrypt_files if encryption
+
+        return_value = yield(params, storage.working_directory)
+
+        FileUtils.rm_rf(storage.working_directory)
+
         return return_value
       end
 
--- lib/fastlane/plugin/match_credentials/helper/my_encrypt.rb
+++ lib/fastlane/plugin/match_credentials/helper/my_encrypt.rb
@@ -2,7 +2,9 @@ require "match"
 
 module Fastlane
   module Helper
-    class CredentialsEncrypt < Match::Encrypt
+    # The iterate method in the superclass only decripts cer, p12 and mobileprovision
+    # Override this method to only decrypt our custom .credential files
+    class CredentialsEncrypt < Match::Encryption::OpenSSL
       def iterate(source_path)
         Dir[File.join(source_path, "**", "*.{credential}")].each do |path|
           next if File.directory?(path)
