01b570fdc5fd6a20087002dd9b59145f30671b05
Felix Scheinost <fesc@symentis.com>
2020-03-27 12:32:23 +0100

    Add spec for setting

--- spec/match_credentials_action_set_spec.rb
+++ spec/match_credentials_action_set_spec.rb
@@ -3,13 +3,20 @@ require "match"
 describe Fastlane::Actions::MatchCredentialsSetAction do
   describe '#run' do
     it 'correctly encrypts the value' do
-      values = {
-        key: "test_credential"
-      }
-      config = FastlaneCore::Configuration.create(Fastlane::Actions::MatchCredentialsSetAction.available_options, values)
-      ENV["MATCH_PASSWORD"] = "123"
-      result = Fastlane::Actions::MatchCredentialsAction.run(config)
-      expect(result).to eq("test_credential_value\n")
+      Dir.mktmpdir do |tmp_origin|
+        system("git clone --bare https://github.com/felixscheinost/fastlane-plugin-match-credentials #{tmp_origin}")
+        values = {
+          git_url: "file:///" + tmp_origin,
+          key: "test_credential2",
+          value: "test_credential2_value"
+        }
+        ENV["MATCH_PASSWORD"] = "123"
+        config = FastlaneCore::Configuration.create(Fastlane::Actions::MatchCredentialsSetAction.available_options, values)
+        Fastlane::Actions::MatchCredentialsSetAction.run(config)
+        Dir.chdir(tmp_origin) do
+          expect(`git log -1 --pretty=%B`.strip).to eq("[fastlane][match_credentials] Set credential 'test_credential2'")
+        end
+      end
     end
   end
 end
