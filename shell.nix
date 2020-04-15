be6a2f19a49f6a8434f5fec5fa086c2f6330abaf
Felix Scheinost <fesc@symentis.com>
2020-04-15 10:44:36 +0200

    Pin nixpkgs in shell.nix
    I want this to still work when I need to fix a bug in 1 year

--- shell.nix
+++ shell.nix
@@ -1,11 +1,20 @@
-{ pkgs ? import <nixpkgs> { } }:
-with pkgs;
+let
+  hostPkgs = import <nixpkgs> {};
+  # nixos-20.03 on 12.04.2020
+  pinnedVersion = hostPkgs.lib.importJSON ./nixpkgs.json;
+  pinnedPkgs = import (hostPkgs.fetchFromGitHub {
+    owner = "NixOS";
+    repo = "nixpkgs-channels";
+    inherit (pinnedVersion) rev sha256;
+  }) {};
+in
+with pinnedPkgs;
 let
   gems = bundlerEnv {
     name = "fastlane-plugin-match_credentials-gems";
     inherit ruby;
     gemdir = ./nix/gem;
   };
-in pkgs.mkShell { 
+in pinnedPkgs.mkShell { 
   buildInputs = [ gems cocoapods ruby bundix ]; 
 }
